3d7571331998fd6fb9f94cf31a017dadec0c7af6
Noor Muhammad <zainnoor6035020@gmail.com>
2024-01-17 17:28:18 +0300

    Setup the communication between frontend and backend. Setup a dummy display of a graph.

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,33 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
+app.use(cors());
 const PORT = 4000;
+const neo4j = require("neo4j-driver");
+
+//CONSTANTS
+const DATABASE = "neo4j";
+const USERNAME = "neo4j";
+const PASSWORD = "Emmawatson00000#";
+
+const driver = neo4j.driver(
+  `bolt://127.0.0.1:7687//${DATABASE}`,
+  neo4j.auth.basic(USERNAME, PASSWORD)
+);
+const session = driver.session();
+
+app.get("/", async (req, res) => {
+  try {
+    const response = await driver.executeQuery(
+      `MATCH path = (initialActor:Person {name: "Tom Hanks"})-[:ACTED_IN*..3]-(connectedActor)
+      WHERE initialActor <> connectedActor 
+      RETURN initialActor, connectedActor, path limit 4`
+    );
+    res.send(response.records);
+  } catch (error) {
+    res.send(error.message);
+  }
+});
 
 app.listen(PORT, (error) => {
   if (!error) {
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,87 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import CytoscapeComponent from "react-cytoscapejs";
+
+const NODE_COLORS = {
+  Movie: "red",
+  Person: "blue",
+  Movie: "red",
+};
 function App() {
+  const [elements, setElements] = useState([]);
+
+  const apiCall = async () => {
+    try {
+      const { data } = await axios.get("http://localhost:4000");
+      console.log(data);
+      var value = [];
+      data.forEach((element) => {
+        let label = element._fields[0].labels[0];
+        let val = {
+          data: {
+            group: "nodes",
+            color: NODE_COLORS[label],
+          },
+        };
+        if (label === "Movie") {
+          val.data.id = element._fields[0].properties.title;
+          val.data.label = element._fields[0].properties.title;
+          val.data.parent = "movie";
+        } else if (label === "Person") {
+          val.data.id = element._fields[0].properties.name;
+          val.data.label = element._fields[0].properties.name;
+          val.data.parent = "person";
+        }
+        value.push(val);
+      });
+      setElements(value);
+    } catch (error) {
+      console.log("ERROR", error);
+    }
+  };
+
+  useEffect(() => {
+    apiCall();
+  }, []);
+  // const elements = [
+  //   { data: { id: "a" } },
+  //   { data: { id: "b" } },
+  //   { data: { id: "c" } },
+  //   { data: { id: "ab", source: "a", target: "b" } },
+  //   { data: { id: "bc", source: "b", target: "c" } },
+  //   { data: { id: "ca", source: "c", target: "a" } },
+  // ];
   return (
     <div className="App">
       <h1>Hello How are you</h1>
+      <button onClick={apiCall}>Make API Call</button>
+      <CytoscapeComponent
+        style={{
+          width: "600px",
+          height: "600px",
+          borderColor: "black",
+          borderWidth: "2px",
+          borderBlockStyle: "double",
+        }}
+        elements={elements}
+        stylesheet={[
+          {
+            selector: "node",
+            style: {
+              "background-color": "data(color)",
+              label: "data(id)",
+            },
+          },
+          {
+            selector: "edge",
+            style: {
+              "line-color": "#95a5a6",
+            },
+          },
+        ]}
+        layout={{ name: "random" }}
+        userZoomingEnabled={false}
+      />
     </div>
   );
 }
