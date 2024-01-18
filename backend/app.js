3c45b3abb52a021c93c8a9fd89886da3a1aca8c2
Noor Muhammad <zainnoor6035020@gmail.com>
2024-01-18 18:53:21 +0300

    Added the graph display, context menu options, extension of graph with context menu

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 app.use(cors());
+app.use(express.json());
 const PORT = 4000;
 const neo4j = require("neo4j-driver");
 
@@ -16,15 +17,79 @@ const driver = neo4j.driver(
 );
 const session = driver.session();
 
-app.get("/", async (req, res) => {
+app.get("/getMovies", async (req, res) => {
+  const { actor } = req.query;
   try {
     const response = await driver.executeQuery(
-      `MATCH path = (initialActor:Person {name: "Tom Hanks"})-[:ACTED_IN*..3]-(connectedActor)
-      WHERE initialActor <> connectedActor 
-      RETURN initialActor, connectedActor, path limit 4`
+      `match (actor:Person {name:"${actor}"})-[:ACTED_IN]->(movie:Movie) return  {name:properties(movie).title,label:"Movie",type:"node"}`
     );
-    res.send(response.records);
+    const output = response.records.map((record) => {
+      return record._fields[0];
+    });
+    res.send(output);
   } catch (error) {
+    console.log("error");
+    res.send(error.message);
+  }
+});
+app.get("/getActors", async (req, res) => {
+  const { movie } = req.query;
+  try {
+    const response = await driver.executeQuery(
+      `match (actor:Person)-[:ACTED_IN]->(movie:Movie {title:"${movie}"}) return {name:properties(actor).name,label:"Person",type:"node"}`
+    );
+    const output = response.records.map((record) => {
+      return record._fields[0];
+    });
+    res.send(output);
+  } catch (error) {
+    console.log("error");
+    res.send(error.message);
+  }
+});
+
+app.get("/", async (req, res) => {
+  const { actor, number } = req.query;
+  try {
+    const response = await driver.executeQuery(
+      `match path=(actor:Person {name:"${actor}"})-[relation:ACTED_IN*1..${
+        number * 2
+      }]-(p:Person) 
+      return 
+      [node in nodes(path)| 
+      case 
+      when labels(node)[0]="Person" then {type:"node",label:labels(node)[0],name:properties(node).name}
+      when labels(node)[0]="Movie" then {type:"node",label:labels(node)[0],name:properties(node).title}
+      end 
+      ] as path`
+    );
+    const nodes = {};
+    const edges = [];
+    response.records.forEach((record) => {
+      var fields = record._fields[0];
+      for (let i = 0; i < fields.length; i++) {
+        let node = fields[i];
+        nodes[node.name] = node;
+        if (node.label === "Person") {
+          if (i < fields.length - 1) {
+            edges.push({
+              from: node.name,
+              to: fields[i + 1].name,
+            });
+          }
+          if (i > 0) {
+            edges.push({
+              from: node.name,
+              to: fields[i - 1].name,
+            });
+          }
+        }
+      }
+      return fields;
+    });
+    res.send({ nodes: Object.values(nodes), edges });
+  } catch (error) {
+    console.log("error");
     res.send(error.message);
   }
 });
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,69 +1,187 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import CytoscapeComponent from "react-cytoscapejs";
+import Cytoscape from "cytoscape";
+import fcose from "cytoscape-fcose";
+import contextMenus from "cytoscape-context-menus";
+Cytoscape.use(fcose);
+Cytoscape.use(contextMenus);
 
 const NODE_COLORS = {
   Movie: "red",
   Person: "blue",
-  Movie: "red",
 };
 function App() {
-  const [elements, setElements] = useState([]);
-
-  const apiCall = async () => {
-    try {
-      const { data } = await axios.get("http://localhost:4000");
-      console.log(data);
-      var value = [];
-      data.forEach((element) => {
-        let label = element._fields[0].labels[0];
-        let val = {
+  const [request, setRequest] = useState({
+    actor: "",
+    number: 0,
+  });
+  var myCyRef = useRef();
+  const renderGraph = (ref, data) => {
+    ref.elements().remove();
+    const { nodes, edges } = data;
+    nodes.forEach((node) => {
+      if (ref.getElementById(node.name).length === 0) {
+        ref.add({
           data: {
-            group: "nodes",
-            color: NODE_COLORS[label],
+            group: node.label,
+            id: node.name,
+            color: NODE_COLORS[node.label],
           },
-        };
-        if (label === "Movie") {
-          val.data.id = element._fields[0].properties.title;
-          val.data.label = element._fields[0].properties.title;
-          val.data.parent = "movie";
-        } else if (label === "Person") {
-          val.data.id = element._fields[0].properties.name;
-          val.data.label = element._fields[0].properties.name;
-          val.data.parent = "person";
-        }
-        value.push(val);
+        });
+      }
+    });
+    edges.forEach((edge) => {
+      if (ref.getElementById(`${edge.from}_${edge.to}`).length === 0) {
+        ref.add({
+          data: {
+            id: `${edge.from}_${edge.to}`,
+            source: edge.from,
+            target: edge.to,
+          },
+        });
+      }
+    });
+    ref.resize();
+    ref.layout({ name: "fcose" }).run();
+  };
+  const apiCall = async () => {
+    if (request.actor === "") {
+      alert("No Actor name provided");
+      return;
+    }
+    if (request.number < 1) {
+      alert("Number value should be greater than 1");
+      return;
+    }
+    try {
+      const { data } = await axios.get("http://localhost:4000", {
+        params: request,
       });
-      setElements(value);
+      renderGraph(myCyRef, data);
     } catch (error) {
       console.log("ERROR", error);
     }
   };
 
+  const getMovies = async (e) => {
+    console.log(e);
+    const actor = e.target.data().id;
+    const { data } = await axios.get("http://localhost:4000/getMovies", {
+      params: { actor },
+    });
+    data.forEach((node) => {
+      if (myCyRef.getElementById(node.name).length === 0) {
+        myCyRef.add({
+          data: {
+            group: node.label,
+            id: node.name,
+            color: NODE_COLORS[node.label],
+          },
+        });
+      }
+      if (myCyRef.getElementById(`${actor}_${node.name}`).length === 0) {
+        myCyRef.add({
+          data: {
+            id: `${actor}_${node.name}`,
+            source: actor,
+            target: node.name,
+          },
+        });
+      }
+    });
+    myCyRef.layout({ name: "fcose" }).run();
+  };
+  const getActors = async (e) => {
+    const movie = e.target.data().id;
+    const { data } = await axios.get("http://localhost:4000/getActors", {
+      params: { movie },
+    });
+    data.forEach((node) => {
+      if (myCyRef.getElementById(node.name).length === 0) {
+        myCyRef.add({
+          data: {
+            group: node.label,
+            id: node.name,
+            color: NODE_COLORS[node.label],
+          },
+        });
+      }
+      if (myCyRef.getElementById(`${node.name}_${movie}`).length === 0) {
+        myCyRef.add({
+          data: {
+            id: `${node.name}_${movie}`,
+            source: node.name,
+            target: movie,
+          },
+        });
+      }
+    });
+    myCyRef.layout({ name: "fcose" }).run();
+  };
+
+  var contextOptions = {
+    evtType: "cxttap",
+    menuItems: [
+      {
+        id: "show_actors",
+        content: "Show Actors",
+        tooltipText: "remove",
+        selector: "node",
+        onClickFunction: getActors,
+        show: true,
+        hasTrailingDivider: true,
+      },
+      {
+        id: "show_movies",
+        content: "Show Movie",
+        tooltipText: "remove",
+        selector: "node",
+        onClickFunction: getMovies,
+        show: true,
+        hasTrailingDivider: true,
+      },
+    ],
+  };
+
   useEffect(() => {
-    apiCall();
+    myCyRef.contextMenus(contextOptions);
   }, []);
-  // const elements = [
-  //   { data: { id: "a" } },
-  //   { data: { id: "b" } },
-  //   { data: { id: "c" } },
-  //   { data: { id: "ab", source: "a", target: "b" } },
-  //   { data: { id: "bc", source: "b", target: "c" } },
-  //   { data: { id: "ca", source: "c", target: "a" } },
-  // ];
   return (
     <div className="App">
       <h1>Hello How are you</h1>
+      <input
+        name="actor"
+        value={request.actor}
+        onChange={(e) =>
+          setRequest({
+            ...request,
+            actor: e.target.value,
+          })
+        }
+      />
+      <input
+        name="number"
+        type="number"
+        value={request.number}
+        onChange={(e) =>
+          setRequest({
+            ...request,
+            number: e.target.value,
+          })
+        }
+      />
       <button onClick={apiCall}>Make API Call</button>
       <CytoscapeComponent
         style={{
-          width: "600px",
-          height: "600px",
+          width: "100%",
+          height: "1024px",
           borderColor: "black",
           borderWidth: "2px",
           borderBlockStyle: "double",
         }}
-        elements={elements}
+        cy={(cy) => (myCyRef = cy)}
+        elements={[]}
         stylesheet={[
           {
             selector: "node",
@@ -80,7 +198,7 @@ function App() {
           },
         ]}
         layout={{ name: "random" }}
-        userZoomingEnabled={false}
+        // userZoomingEnabled={false}
       />
     </div>
   );
